1ffba6b19ca0f02627d73809a9939e22c918cd67
crismillan <crismillanc06@gmail.com>
2025-11-27 12:00:36 +0100

    Delete initial directory

--- initial/ubuntu_image.tf
+++
@@ -1,7 +0,0 @@
-resource "openstack_images_image_v2" "ubuntu" {
-  name             = "ubuntu-22.04"
-  disk_format      = "qcow2"
-  container_format = "bare"
-  visibility       = "public"
-  image_source_url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-}
